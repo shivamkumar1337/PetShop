7c42a9203d8aa7208633eda0fd24e853001dca43
Kartikeya-sekisho <kartikeya_singh@sekisho.co.jp>
2025-07-03 11:00:33 +0900

    thursday

--- sql/init.sql
+++ sql/init.sql
@@ -7,6 +7,7 @@ CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Table: customers
